485429156c754ae8c9c38a357427b3299f39ebe3
diyaps02 <140656509+diyaps02@users.noreply.github.com>
2025-06-03 17:13:59 +0530

    last change

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: "https://mini-messanger04.onrender.com", 
   credentials: true               
 }));
 
